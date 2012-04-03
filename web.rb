39ed57c5bdb25ae088d4873763d2c1d13adf4e3c
Gareth Fleming <d3z@joocy.com>
2012-04-03 02:46:10 +0100

    added redis dependency

--- web.rb
+++ web.rb
@@ -1,5 +1,14 @@
+require 'rubygems'
 require 'sinatra'
+require 'redis'
+
+uri = URI.parse(ENV["REDISTOGO_URL"]);
+redis = Redis.new :host=>uri.host, :port=>uri.port, :password=>uri.password
 
 get '/' do
     'Hello to Plusone'
+end
+
+get '/:handle' do
+    
 end
